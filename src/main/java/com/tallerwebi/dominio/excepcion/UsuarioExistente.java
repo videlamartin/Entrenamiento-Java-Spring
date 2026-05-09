b7b5bd09b3ee3af4998a8419373fd6f7c2b4ac28
Martin Videla <videlamartin1905@gmail.com>
2026-05-09 15:18:26 -0300

    Ejercicio 1 - Formulario de contacto

--- src/main/java/com/tallerwebi/dominio/excepcion/UsuarioExistente.java
+++
@@ -1,7 +0,0 @@
-package com.tallerwebi.dominio.excepcion;
-
-public class UsuarioExistente extends Exception {
-
-  /* Identificador para la serialización de la clase, requerido por PMD en excepciones */
-  private static final long serialVersionUID = 1L;
-}
