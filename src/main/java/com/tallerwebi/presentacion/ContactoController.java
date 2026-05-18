ceea50f1bb3064a646ada632d02647a3f6ab88a6
Martin Videla <videlamartin1905@gmail.com>
2026-05-17 23:27:25 -0300

    Entrenamiento 2 - Contacto

--- src/main/java/com/tallerwebi/presentacion/ContactoController.java
+++ src/main/java/com/tallerwebi/presentacion/ContactoController.java
@@ -5,23 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/contacto")
 public class ContactoController {
 
   private static final String VISTA_CONTACTO = "contacto";
 
-  @GetMapping
+  @GetMapping("/contacto")
   public ModelAndView mostrarFormulario() {
     ModelAndView mav = new ModelAndView(VISTA_CONTACTO);
     mav.addObject(VISTA_CONTACTO, new Contacto());
     return mav;
   }
 
-  @PostMapping
+  @PostMapping("/contacto")
   public ModelAndView procesarFormulario(@ModelAttribute Contacto contacto) {
     ModelAndView mav = new ModelAndView("confirmacion");
     mav.addObject(VISTA_CONTACTO, contacto);
